0bca9cb1120618b6ac217c1d9ea908214ba60fe8
Jason Burkybile <jburky@gmail.com>
2021-02-17 13:22:28 -0500

    Fixed an issue with null check on dropdown selection

---
+++ Springbrook/Shared/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Springbrook.Shared
+{
+    public static class Extensions
+    {
+        public static bool IsNullOrWhitespace(this string item)
+        {
+            return String.IsNullOrWhiteSpace(item);
+        }
+    }
+}
